b36f91a511e069ecbac5f8e9bcb09448662e9607
theniteshnarang <narangnitesh@ymail.com>
2021-05-13 16:52:06 +0530

    Add: Added responsiveness to docs page

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const modalOpen = document.querySelector('.modal-open')
 const modalClose = document.querySelectorAll('.modal-close')
 const modal = document.querySelector('.modal')
 const body = document.querySelector('body')
+const menu = document.querySelector('#menu')
+const components = document.querySelector('#components')
+modalOpen?.addEventListener('click', toggleModal)
 
-modalOpen.addEventListener('click', toggleModal)
+menu.addEventListener('click', toggleComponents)
+
 modalClose.forEach( close => close.addEventListener('click', toggleModal))
 
 function toggleModal(){
     body.classList.toggle('hide-scroll')
     return modal.classList.toggle('modal-hide')
+}
+
+function toggleComponents(){
+    return components.classList.toggle('components-show')
 }
